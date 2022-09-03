4f468fb39bfa9fd39f3d837f9f0094a9006cd4a4
hoangtien2911 <hoangtienbmt2911@gmail.com>
2022-09-03 18:23:40 +0700

    first time

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to my Repository - Shoes-store project
+* Web201c - FALL2022 
+* This is my first project with html/css/js and this project i study in youtube.
+
+## © 2021 Hoang Tien | fb/htmsyunh/
---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+/*===== MENU SHOW =====*/
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+/*===== REMOVE MENU =====*/
+const removeMenu = (navMenuId, navLinkClass) => {
+    const navMenu = document.getElementById(navMenuId),
+          navLink = document.querySelectorAll(navLinkClass)
+
+    navLink.forEach(n => n.addEventListener('click', () => {
+        navMenu.classList.remove('show')
+    }))
+}
+removeMenu('nav-menu', '.nav__link')
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active')
+		}else{
+			sectionsClass.classList.remove('active')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== CHANGE COLOR HEADER =====*/
+window.onscroll = () => {
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200)
+        nav.classList.add('scroll-header');
+    else
+        nav.classList.remove('scroll-header')
+}
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
